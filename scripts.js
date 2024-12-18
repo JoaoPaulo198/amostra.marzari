5bcd7e9aeb6da5f200b3388420572d8acf49ec53
JoaoPaulo198 <joaopaulos.198@outlook.com>
2024-12-18 03:04:17 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,11 @@
+let imagem = document.querySelector(".imagem-copo")
+
+let circulo = document.querySelector(".circulo")
+
+       function trocaImagem (trocar){      
+              imagem.src = trocar
+       }
+        
+       function trocaCor (cor){
+              circulo.style.background = cor
+       }
